041b9249bfc8d55e21ed082608978afdc3e40263
renanhangai <hangai.renan@gmail.com>
2017-11-10 14:25:49 -0200

    Added new rules for many types

--- src/validator/RuleDefinition.php
+++ src/validator/RuleDefinition.php
@@ -32,36 +32,133 @@ class RuleDefinition {
 		throw new \InvalidArgumentException( "Invalid rule: ".$name );
 	}
 
-	public static function s( $value, $trim = true ) {
-		$value = (string) $value;
-		if ( $trim )
-			$value = trim( $value );
-		return $value;
-	}
-	
+	/*
+	  
+	 */
 	public static function optional__raw( $state ) {
 		if ( ( $state->value === null ) || ( $state->value === '' ) ) {
 			$state->value = null;
-			$state->setDone( true );
+			$state->setDone();
 		}
-	}
-	
-	public static function dependsOn__raw( $state ) {
-	}
-	public static function dependsOn__rawSetup( $state, $fields ) {
-		if ( is_string( $fields ) )
-			$state->addDependency( $fields );
-		else if ( is_array( $fields ) ) {
-			foreach ( $fields as $field )
-				$state->addDependency( $field );
+	}	
+	public static function required__raw( $state ) {
+		if ( ( $state->value === null ) || ( $state->value === '' ) ) {
+			$state->value = null;
+			$state->addError( "Field is required" );
+			$state->setDone();
+		}
+	}	
+	public static function skippable__raw( $state ) {
+		if ( ( $state->value === null ) || ( $state->value === '' ) ) {
+			$state->value = null;
+			$state->setFlag( State::FLAG_SKIP );
+			$state->setDone();
 		}
 	}
 
+
+	/// Add a rule dependency
+	public static function dependsOn__raw( $state ) {
+	}
+	public static function dependsOn__rawSetup( $state, $fields ) {
+		$state->dependsOn( $fields );
+	}
+
+	/// call
 	public static function call__factory( $fn ) {
 		return new rule\RuleInline( $fn, array(), 'call' );
 	}
-	
+
+	/// obj
 	public static function obj__factory( $definition ) {
 		return new rule\RuleObject( $definition );
 	}
+
+	
+	/*
+	  Type validators
+	 */
+	// Any validator
+	public static function any( $value ) {
+		return true;
+	}
+	// String value
+	public static function strval( $value, $trim = true ) {
+		return self::s( $value, $trim );
+	}
+	public static function s( $value, $trim = true ) {
+		if ( is_object( $value ) ) {
+			if ( !method_exists( $value, '__toString' ) )
+				throw RuleException::createWithValue( "Object cannot be converted to string.", $value );
+			$value = (string) $value;
+		}
+		if ( $trim !== false )
+			return trim( $value );
+		else
+			return strval( $value );
+	}
+	// Int value
+	public static function intval( $value ) {
+		return self::i( $value );
+	}
+	public static function i( $value ) {
+		$error = false;
+		if ( is_int( $value ) ) {
+		    return true;
+		} else if ( is_string( $value ) ) {
+			if ( !ctype_digit( $value ) )
+				throw RuleException::createWithValue( "Value must be an int.", $value );
+			return intval( trim( $value ), 10 );
+		} else
+			throw RuleException::createWithValue( "Value must be an int.", $value );
+	}
+
+	// Float value
+	public static function floatval( $value, $decimal = null, $thousands = null ) {
+		return self::f( $value, $decimal, $thousands );
+	}
+	public static function f( $value, $decimal = null, $thousands = null ) {
+		$error = false;
+		if ( $decimal === null )
+			$decimal   = '.';
+
+		if ( is_int( $value ) ) {
+		    return true;
+		} else if ( is_float( $value ) ) {
+		    return true;
+		} else if ( is_string( $value ) ) {
+			$value = trim( $value );
+			$isNegative = ( @$value[0] === '-' );
+			if ( $isNegative )
+				$value = substr( $value, 1 );
+			if ( $thousands )
+				$value = str_replace( $thousands, "", $value );
+			if ( ctype_digit( $value ) ) {
+				$value = intval( $value, 10 );
+				return $isNegative ? -$value : $value;
+			}
+			$split = explode( $decimal, $value );
+			if ( ( count($split) != 2 ) || ( !ctype_digit( $split[0] ) ) || ( !ctype_digit( $split[1] ) ) )
+				throw RuleException::createWithValue( "Value must be a float.", $value );
+			$value = ( $decimal === '.' ) ? floatval( $value ) : floatval( $split[0].'.'.$split[1] );
+			return $isNegative ? -$value : $value;
+		} else
+			throw RuleException::createWithValue( "Value must be a float.", $value );
+	}
+	// Boolean value
+	public static function boolval( $value ) {
+		return self::b( $value );
+	}
+	public static function b( $value ) {
+		if ( !$value || ($value === 'false') )
+		    return new rule\InlineRuleValue( false );
+		else if ( ( $value === true ) || ( $value === 'true' ) || ( $value == '1' ) )
+		    return new rule\InlineRuleValue( true );
+		else
+			throw RuleException::createWithValue( "Value must be a boolean.", $value );
+
+	}
+
+
+
 };

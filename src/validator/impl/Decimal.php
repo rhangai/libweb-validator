e3889f96b05861c0a1d684768706f56245c64bae
renanhangai <hangai.renan@gmail.com>
2017-11-10 18:29:41 -0200

    Added implementation of Decimal class

---
+++ src/validator/impl/Decimal.php
@@ -0,0 +1,18 @@
+<?php
+namespace LibWeb\validator\impl;
+
+/**
+ * Implements the Decimal class compatible with LibWeb
+ */
+class Decimal extends \RtLopez\DecimalBCMath {
+
+	/// Convert to string
+	public function __toString() {
+		return $this->serializeAPI();
+	}
+	/// Serialize this decimal using the following formatters
+	public function serializeAPI() {
+		return $this->format( null, '.', '' );
+	}
+	
+};

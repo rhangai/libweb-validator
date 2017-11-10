e00b2ba52e5378cd35469057d037358083168d3d
renanhangai <hangai.renan@gmail.com>
2017-11-10 09:38:06 -0200

    Added RuleBuilder to build the rules

---
+++ src/validator/rule/RuleBuilder.php
@@ -0,0 +1,39 @@
+<?php
+namespace LibWeb\validator\rule;
+
+use LibWeb\Validator;
+use LibWeb\validator\Rule;
+
+class RuleBuilder implements Rule {
+
+	// Apply the rules
+	public function setup( $state ) {
+		foreach ( $this->chain_ as $rule ) {
+			$rule->setup( $state );
+		}
+	}
+	// Apply the rules
+	public function apply( $state ) {
+		foreach ( $this->chain_ as $rule ) {
+		    $rule->apply( $state );
+			if ( $state->isDone() )
+				break;
+		}
+	}
+	// Apply the rules
+	public function __call( $method, $args ) {
+		$rule = RuleDefinition::get( $method, $args );
+		$this->appendRule( $rule );
+		return $this;
+	}
+	// Add a new rule to the beginning of the chain
+	public function prependRule( $rule ) {
+		array_unshift( $this->chain_, $rule );
+	}
+	// Append a new rule to the end of the chain
+	public function appendRule( $rule ) {
+		$this->chain_[] = $rule;
+	}
+	// Chain
+	private $chain_ = array();
+};

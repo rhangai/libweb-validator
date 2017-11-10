a2e394091a5dfae4a0aec949abfac71846d17efd
renanhangai <hangai.renan@gmail.com>
2017-11-10 09:37:43 -0200

    Updated state implementation

--- src/validator/State.php
+++ src/validator/State.php
@@ -3,30 +3,80 @@ namespace LibWeb\validator;
 
 class State {
 
+	/// Value variable
 	public $value;
-
+	/**
+	 * Construct the state
+	 */
 	public function __construct( $value, $key = null, $parent = null ) {
 		$this->value    = $value;
 		$this->initial_ = $value;
 		$this->key_     = self::mergeKeys( $key, $parent );
 		$this->parent_  = $parent;
 	}
-
+	/// Get the key
 	public function getKey() { return $this->key_; }
-
+	/// Get the parent
 	public function getParent() { return $this->parent_; }
-
+	/// Get the initial value for the state
+	public function getInitial() { return $this->initial_; }
+	/// Mark the state as finished
+	public function setDone( $value = true ) { $this->done_ = !!$value; }
+	/// Check if state has finished
+	public function isDone() { return $this->done_; }
+	/**
+	 * Merge keys for state
+	 */
 	private static function mergeKeys( $key, $parent ) {
 		if ( !$parent )
-			return $key ? array( $key ) : array();
-
+			return $key !== null ? array( $key ) : array();
 		$keys = $parent->getKey();
-		if ( $key )
+		if ( $key !== null )
 			$keys[] = $key;
 		return $keys;
 	}
-	
+	/**
+	 * Get validator sub-data
+	 */
+	public function validatorGet( $key ) {
+		if ( $this->getter_ === null )
+			$this->getter_ = self::createGetterFor( $this->initial_ );
+		if ( $this->getter_ === false )
+			throw new \LogicException( "Cannot get any sub key on this state." );
+		return $this->getter_->validatorGet( $key );
+	}
+	/**
+	 * Create a dependency on the given key
+	 */
+	public function dependsOn( $key ) {
+		$this->dependencies_[] = $key;
+	}
+	/**
+	 * Get the dependencies on the state
+	 */
+	public function getDependencies() {
+		return $this->dependencies_;
+	}
+	/**
+	 * Create a getter for subdata to validate
+	 */
+	private static function createGetterFor( $value ) {
+		if ( is_object( $value ) ) {
+			if ( ( $value instanceof \stdClass ) || ( $value instanceof \ArrayAccess ) )
+				return new getter\ArrayGetter( $value );
+			else if ( is_callable( array( $value, 'validatorGet' ) ) )
+				return $value;
+			return false;
+		} else if ( is_array( $value ) ) {
+			return new getter\ArrayGetter( $value );
+		}
+		return false;
+	}
+
 	private $initial_;
 	private $key_;
 	private $parent_;
+	private $getter_;
+	private $done_;
+	private $dependencies_ = array();
 };
--- src/validator/State.spec.php
+++ src/validator/State.spec.php
@@ -9,14 +9,38 @@ class TestState extends TestCase {
 		$value = new \stdClass;
 		$state = new State( $value );
 		
-		$this->assertEquals( $value, $state->value );
+		$this->assertSame( $value, $state->value );
 	}
 
-	public function testParent() {
-		$value = new \stdClass;
-		$state = new State( $value );
+	public function testParents() {
+		$levels = 10;
+		$states = array( new State( null ) );
+		for ( $i = 0; $i < $levels; ++$i ) {
+			$states[] = new State( null, $i, $states[ $i ] );
+		}
+		for ( $i = 0; $i < $levels; ++$i ) {
+			$this->assertSame( $states[ $i ], $states[ $i + 1 ]->getParent() );
+		}
 		
-		$this->assertEquals( $value, $state->value );
+		$key = array();
+		for ( $i = 0; $i < $levels; ++$i ) {
+			$key[] = $i;
+			$this->assertEquals( $key, $states[ $i + 1 ]->getKey() );
+		}
+		
+	}
+
+
+	public function testValidatorGet() {
+		$value = array(
+			0 => 0,
+			"test" => uniqid(),
+		);
+		$state = new State( (object) $value );
+
+		foreach ( $value as $key => $v ) {
+			$this->assertEquals( $v, $state->validatorGet( $key ) );
+		}
 	}
 
 };

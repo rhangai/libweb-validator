bdbe9c4af2ac7b54cf90c2f2bdb66edea3868925
renanhangai <hangai.renan@gmail.com>
2017-11-10 09:37:27 -0200

    Added ArrayGetter class

--- src/validator/getter/ArrayGetter.php
+++ src/validator/getter/ArrayGetter.php
@@ -1,15 +1,19 @@
 <?php
 namespace LibWeb\validator\getter;
 
-class ArrayGetter {
+/**
+ * Get the keys on an array (or array like object)
+ */
+class ArrayGetter implements Getter {
 
+	/// Construct the getter by passing the array
 	public function __construct( $array ) {
 		$this->array_ = (array) $array;
 	}
-	
+	/// Get a key on the array
 	public function validatorGet( $key ) {
 		return @$this->array_[ $key ];
 	}
 
-	private $array_;
+	private $array_; ///< The array
 }

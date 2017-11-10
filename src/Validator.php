d542a0a2a55240d05584582c9009b45d2cee0e31
renanhangai <hangai.renan@gmail.com>
2017-11-10 10:10:02 -0200

    Added throw of validator rules

--- src/Validator.php
+++ src/Validator.php
@@ -1,10 +1,38 @@
 <?php
 namespace LibWeb;
 
+/**
+ * Validator main class
+ */
 class Validator {
 
+	/// Validate a value against a rule
 	public static function validate( $value, $rule ) {
-		
+		$state = new validator\State( $value );
+		self::validateState( $state, $rule );
+		if ( $state->getErrors() )
+			throw new ValidatorException( $state );
+		return $state->value;
+	}
+	/// Validate the state object
+	public static function validateState( $state, $rule ) {
+		$rule = self::normalizeRule( $rule );
+		$rule->apply( $state );
+	}
+	/// Normalize a rule
+	public static function normalizeRule( $rule ) {
+		if ( is_array( $rule ) )
+			$rule = new validator\rule\RuleObject( $rule );
+		else if ( !$rule instanceof validator\Rule )
+			throw new \InvalidArgumentException( "Rule must be an array or an instance of Rule" );
+		return $rule;
+	}
+	/**
+	 * Call a static rule on validatr
+	 */
+	public static function __callStatic( $name, $args ) {
+		$builder = new validator\rule\RuleBuilder;
+		return $builder->__call( $name, $args );
 	}
 	
 };

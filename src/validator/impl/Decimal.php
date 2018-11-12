fa87a95d896ae6aef1c50547efc81ef5bc94842f
renanhangai <hangai.renan@gmail.com>
2018-11-12 16:26:59 -0200

    chore: Added jsonSerializable on Decimal implementation

--- src/validator/impl/Decimal.php
+++ src/validator/impl/Decimal.php
@@ -4,15 +4,16 @@ namespace libweb\validator\impl;
 /**
  * Implements the Decimal class compatible with libweb
  */
-class Decimal extends \RtLopez\DecimalBCMath {
+class Decimal extends \RtLopez\DecimalBCMath implements \JsonSerializable {
 
 	/// Convert to string
 	public function __toString() {
-		return $this->serializeAPI();
-	}
-	/// Serialize this decimal using the following formatters
-	public function serializeAPI() {
 		return $this->format( null, '.', '' );
 	}
-	
+	public function serializeAPI() {
+		return $this->__toString();
+	}
+	public function jsonSerialize() {
+		return $this->__toString();
+	}
 };
